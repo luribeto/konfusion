d6c7a80f530da7a51ec153edeeb038fe11b5cb16
Leonardo Uribe <luribeto@gmail.com>
2018-11-26 09:58:24 -0500

    Initial Setup

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>MAILORS te amare por siempre hasta el infinito y mas alla!!</Text>
+        <Text>TE AMO!</Text>
+        <Text>Otro TEXT</Text>
+        <Text>TEST</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
