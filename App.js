fa4756c82951f7396867d8250f60061ff81f177c
Leonardo Uribe <luribeto@gmail.com>
2018-11-26 18:10:14 -0500

    List of dishes

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import Main from './components/MainComponent';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>This is an REACT NATIVE APP</Text>
+//         <Text>Edit App.js file</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#ccc',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is an REACT NATIVE APP</Text>
-        <Text>Edit App.js file</Text>
-      </View>
+      <Main style={styles.container} />
     );
   }
 }
@@ -15,7 +37,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
