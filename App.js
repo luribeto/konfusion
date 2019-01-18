e198a8ce0ccc695f8d7caf3af05b17ba0636ad0f
Leonardo Uribe <luribeto@gmail.com>
2019-01-18 11:28:50 -0500

    Persist Redux Store

--- App.js
+++ App.js
@@ -27,14 +27,20 @@ import { StyleSheet, View } from 'react-native'
 import Main from './components/MainComponent'
 import { Provider } from 'react-redux'
 import { ConfigureStore } from './redux/configureStore'
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './components/LoadingComponent';
 
-const store = ConfigureStore()
+const { persistor, store } = ConfigureStore();
+
+// const store = ConfigureStore()
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Main style={styles.container} />
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <Main style={styles.container} />
+        </PersistGate>
       </Provider>
     );
   }
