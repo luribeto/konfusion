14019ebcc51c2e42502a9040d940f1e071661c0f
Leonardo Uribe <luribeto@gmail.com>
2019-01-18 10:55:52 -0500

    Gestures Part 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -58,6 +58,8 @@ function RenderComments(props) {
 function RenderDish(props) {
   const dish = props.dish
 
+  handleViewRef = ref => this.view = ref;
+
   const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
     if ( dx < -200 ) return true;
     return false;
@@ -66,6 +68,11 @@ function RenderDish(props) {
   const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (e, gestureState) => {
           return true;
+      },
+      onPanResponderGrant: () => {
+        debugger
+        this.view.rubberBand(1000)
+        .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
       },
       onPanResponderEnd: (e, gestureState) => {
           console.log("pan responder end", gestureState);
@@ -88,7 +95,10 @@ function RenderDish(props) {
     const imageUri = IMAGES_MAP[dish.image]
 
     return (
-      <Animatable.View animation="fadeInDown" duration={1000} delay={500} {...panResponder.panHandlers}>
+      <Animatable.View animation="fadeInDown" duration={1000} delay={500} 
+          {...panResponder.panHandlers}
+          ref={this.handleViewRef}
+        >
         <Card
           featuredTitle={dish.name}
           image={{ uri: imageUri.url }}>
